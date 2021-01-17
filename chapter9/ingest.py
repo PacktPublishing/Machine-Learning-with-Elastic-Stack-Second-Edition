8a2230ceaaff1802c55764e9da3af3848eee26d3
Winterflower <camillamon@gmail.com>
2021-01-17 23:15:32 +0100

    Adds initial skeleton for ingest script

---
+++ chapter9/ingest.py
@@ -0,0 +1,14 @@
+"""
+This is a script that accompanies Chapter 9
+from the Packt book Machine Learning with Elastic, 2nd Edition.
+Please see the README for more information.
+"""
+
+from elasticsearch import Elasticsearch, helpers
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
