dd298bf2b962fb2271e63e0128986bb82c25e9bd
Winterflower <camillamon@gmail.com>
2021-01-18 23:44:45 +0100

    Adds initial skeleton script for ingesting data

--- chapter9/ingest.py
+++ chapter9/ingest.py
@@ -4,11 +4,27 @@ from the Packt book Machine Learning with Elastic, 2nd Edition.
 Please see the README for more information.
 """
 
+import argparse
+import os
+
 from elasticsearch import Elasticsearch, helpers
 
+def parse_args():
+    """
+    Helper method for parsing CLI args
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--es', type=str, help="URL of ES instance")
+    return parser.parse_args()
 
 def main():
-    pass
+    args = parse_args()
+    # credentials for your ES instance
+    es_username = os.env['ES_USERNAME']
+    es_password = os.env['ES_PASSWORD']
+
+    es_client = Elasticsearch(args.es, http_auth=(es_username, es_password))
+
 
 if __name__ == '__main__':
     main()
